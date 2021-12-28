762a41b2ca4c759468fa4bd049604f8791ddb677
Daniel Litt <daniel_litt@comcast.net>
2021-12-27 21:43:14 -0500

    Whitespace and indentation fix

--- src/morse-code-watch/morse-code-watch.ino
+++ src/morse-code-watch/morse-code-watch.ino
@@ -3,74 +3,74 @@ int touchSensorPin = 3;
 
 void setup()
 {
-  pinMode(vibrationMotorPin, OUTPUT);
-  pinMode(touchSensorPin, INPUT);
+    pinMode(vibrationMotorPin, OUTPUT);
+    pinMode(touchSensorPin, INPUT);
 }
 
 void loop()
 {
-  onTouched(vibrateTestMorseDigit);
+    onTouched(vibrateTestMorseDigit);
 }
 
 int lastTouchedState = 0;
 
 void onTouched(void (*touchedAction)())
 {
-  int touchSensorState = digitalRead(touchSensorPin);
+    int touchSensorState = digitalRead(touchSensorPin);
 
-  if (touchSensorState != lastTouchedState && touchSensorState == HIGH)
-    touchedAction();
+    if (touchSensorState != lastTouchedState && touchSensorState == HIGH)
+        touchedAction();
 
-  lastTouchedState = touchSensorState;
+    lastTouchedState = touchSensorState;
 }
 
 void vibrateTestMorseDigit()
 {
-  vibrateMorseDigit(4);
+    vibrateMorseDigit(4);
 }
 
 int dotDuration = 100;
-int dashDuration = dotDuration*3;
+int dashDuration = dotDuration * 3;
 int betweenDotDashDelay = dotDuration;
-int intraDigitDelay = dotDuration*3;
+int intraDigitDelay = dotDuration * 3;
 
 char morseDigits[10][5] = {
-  "-----",
-  ".----",
-  "..---",
-  "...--",
-  "....-",
-  ".....",
-  "-....",
-  "--...",
-  "---..",
-  "----."
-};
+    "-----",
+    ".----",
+    "..---",
+    "...--",
+    "....-",
+    ".....",
+    "-....",
+    "--...",
+    "---..",
+    "----."};
 
 void vibrateMorseDigit(int digit)
 {
-  if (digit >= 10) return;
+    if (digit >= 10)
+        return;
 
-  for (auto dotOrDash : morseDigits[digit])
-  {
-    (dotOrDash == '-') ? dash() : dot();
-    delay(betweenDotDashDelay);
-  }
+    for (auto dotOrDash : morseDigits[digit])
+    {
+        (dotOrDash == '-') ? dash() : dot();
+        delay(betweenDotDashDelay);
+    }
 }
 
 void dot()
 {
-  vibrateForMilliseconds(dotDuration);
+    vibrateForMilliseconds(dotDuration);
 }
 
 void dash()
 {
-  vibrateForMilliseconds(dashDuration);
+    vibrateForMilliseconds(dashDuration);
 }
 
 void vibrateForMilliseconds(int milliseconds)
 {
-  digitalWrite(vibrationMotorPin, HIGH);
-  delay(milliseconds);
-  digitalWrite(vibrationMotorPin, LOW);
+    digitalWrite(vibrationMotorPin, HIGH);
+    delay(milliseconds);
+    digitalWrite(vibrationMotorPin, LOW);
 }
