759b8e864ad84429fa5ab8905bc131f29f25d8dc
Daniel Litt <daniel_litt@comcast.net>
2021-12-27 20:14:27 -0500

    Add code to vibrate morse code digits

--- src/morse-code-watch/morse-code-watch.ino
+++ src/morse-code-watch/morse-code-watch.ino
@@ -22,3 +22,51 @@ void onTouched(void (*touchedAction)())
 
   lastTouchedState = touchSensorState;
 }
+
+int dotDuration = 250;
+int dashDuration = dotDuration*3;
+int betweenDotDashDelay = dotDuration;
+int intraDigitDelay = dotDuration*3;
+
+char morseDigits[10][5] = {
+  "-----",
+  ".----",
+  "..---",
+  "...--",
+  "....-",
+  ".....",
+  "-....",
+  "--...",
+  "---..",
+  "----." 
+};
+
+void vibrateMorseDigit(int digit)
+{
+  if (digit >= 10) return;
+
+  char dotsAndDashes[5] = { morseDigits[digit] };
+
+  for (auto dotOrDash : dotsAndDashes)
+  {
+    (dotOrDash == ".") ? dot() : dash();
+    delay(betweenDotDashDelay);
+  }
+}
+
+void dot() 
+{
+  vibrateForMilliseconds(dotDuration);
+}
+
+void dash()
+{
+  vibrateForMilliseconds(dashDuration);
+}
+
+void vibrateForMilliseconds(int milliseconds)
+{
+  digitalWrite(vibrationMotorPin, HIGH);
+  delay(milliseconds);
+  digitalWrite(vibrationMotorPin, LOW);
+}
