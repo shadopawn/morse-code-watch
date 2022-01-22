c34d11e7d288c0f9ef3a47e26416e486a297e5fc
Daniel Litt <daniel_litt@comcast.net>
2022-01-22 13:37:29 -0500

    Add SoftwareSerial to read-internal-voltage.ino

--- examples/read-internal-voltage/read-internal-voltage.ino
+++ examples/read-internal-voltage/read-internal-voltage.ino
@@ -1,5 +1,13 @@
+#include <SoftwareSerial.h>
+
+#define RX 3 // PB3, Pin 2
+#define TX 4 // PB4, Pin 3
+
+SoftwareSerial Serial(RX, TX);
+
 void setup()
 {
+    Serial.begin(9600);
 }
 
 void loop()
