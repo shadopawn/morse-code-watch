d862291f0ec45f8d1a1bbff5f0b759d3020246bd
Daniel Litt <daniel_litt@comcast.net>
2022-01-22 13:48:09 -0500

    Add internalVoltage function

--- examples/read-internal-voltage/read-internal-voltage.ino
+++ examples/read-internal-voltage/read-internal-voltage.ino
@@ -12,4 +12,24 @@ void setup()
 
 void loop()
 {
+    Serial.println(internalVoltage());
+    delay(1000);
+}
+
+int internalVoltage()
+{
+    // reads internal 1V1 reference against VCC
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+    delay(2);            // Wait for Vref to settle
+    ADCSRA |= _BV(ADSC); // Convert
+    while (bit_is_set(ADCSRA, ADSC));
+    uint8_t low = ADCL;
+    unsigned int val = (ADCH << 8) | low;
+    // discard previous result
+    ADCSRA |= _BV(ADSC); // Convert
+    while (bit_is_set(ADCSRA, ADSC));
+    low = ADCL;
+    val = (ADCH << 8) | low;
+    // returns voltage in mV
+    return ((long)1024 * 1100) / val;
 }
